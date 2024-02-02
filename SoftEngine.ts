4c0383bb972a3949cf5b5e81114ecfb947d795cc
Anon <opss59332@gmail.com>
2024-02-02 18:18:54 +0530

    basic texturing and z-buffering done

--- SoftEngine.ts
+++ SoftEngine.ts
@@ -29,7 +29,7 @@ module SoftEngine {
       this.Faces = new Array(faceCount);
       this.Rotation = BABYLON.Vector3.Zero();
       this.Position = BABYLON.Vector3.Zero();
-      this.UVs = new Array(verticesCount)
+      this.UVs = new Array(verticesCount);
     }
   }
 
@@ -40,12 +40,14 @@ module SoftEngine {
     private workingWidth: number;
     private workingHeight: number;
     private backbufferdata;
+    private depthbuffer: number[];
 
     constructor(canvas: HTMLCanvasElement) {
       this.workingCanvas = canvas;
       this.workingWidth = canvas.width;
       this.workingHeight = canvas.height;
       this.workingContext = this.workingCanvas.getContext("2d")!;
+      this.depthbuffer = new Array(this.workingWidth * this.workingHeight);
     }
 
     public clear(): void {
@@ -61,16 +63,29 @@ module SoftEngine {
         this.workingWidth,
         this.workingHeight
       );
+
+      for (let i = 0; i < this.depthbuffer.length; i++) {
+        this.depthbuffer[i] = 1000000;
+      }
     }
 
     public present(): void {
       this.workingContext.putImageData(this.backbuffer, 0, 0);
     }
 
-    public putPixel(x: number, y: number, color: BABYLON.Color4): void {
+    public putPixel(
+      x: number,
+      y: number,
+      z: number,
+      color: BABYLON.Color4
+    ): void {
       this.backbufferdata = this.backbuffer.data;
       let index: number = ((x >> 0) + (y >> 0) * this.workingWidth) * 4;
       //   console.log("putting pixel")
+      if (this.depthbuffer[index] < z) {
+        return;
+      }
+      this.depthbuffer[index] = z;
 
       this.backbufferdata[index] = color.r * 255;
       this.backbufferdata[index + 1] = color.g * 255;
@@ -81,51 +96,137 @@ module SoftEngine {
     public project(
       coord: BABYLON.Vector3,
       transMat: BABYLON.Matrix
-    ): BABYLON.Vector2 {
+    ): BABYLON.Vector3 {
       let point = BABYLON.Vector3.TransformCoordinates(coord, transMat);
 
       let x = (point.x * this.workingWidth + this.workingWidth / 2.0) >> 0;
       var y = (-point.y * this.workingHeight + this.workingHeight / 2.0) >> 0;
 
-      return new BABYLON.Vector2(x, y);
+      return new BABYLON.Vector3(x, y, point.z);
     }
 
-    public drawPoint(point: BABYLON.Vector2): void {
+    public drawPoint(point: BABYLON.Vector3, color: BABYLON.Color4): void {
       if (
         point.x >= 0 &&
         point.y >= 0 &&
         point.x < this.workingWidth &&
         point.y < this.workingHeight
       ) {
-        this.putPixel(point.x, point.y, new BABYLON.Color4(1, 1, 0, 1));
+        this.putPixel(point.x, point.y, point.z, color);
       }
     }
 
-    public drawBLine(point0: BABYLON.Vector2, point1: BABYLON.Vector2): void {
-      let x0 = point0.x >> 0;
-      let y0 = point0.y >> 0;
-      let x1 = point1.x >> 0;
-      let y1 = point1.y >> 0;
-      let dx = Math.abs(x1 - x0);
-      let dy = Math.abs(y1 - y0);
-      let sx = x0 < x1 ? 1 : -1;
-      let sy = y0 < y1 ? 1 : -1;
-      let err = dx - dy;
+    public clamp(value: number, min: number = 0, max: number = 1): number {
+      return Math.max(min, Math.min(value, max));
+    }
 
-      while (true) {
-        this.drawPoint(new BABYLON.Vector2(x0, y0));
-        if (x0 == x1 && y0 == y1) break;
-        let e2 = 2 * err;
-        if (e2 > -dy) {
-          err -= dy;
-          x0 += sx;
+    public interpolate(min: number, max: number, gradient: number) {
+      return min + (max - min) * this.clamp(gradient);
+    }
+
+    public processScanLine(
+      y: number,
+      pa: BABYLON.Vector3,
+      pb: BABYLON.Vector3,
+      pc: BABYLON.Vector3,
+      pd: BABYLON.Vector3,
+      color: BABYLON.Color4
+    ): void {
+      let gradient1 = pa.y != pb.y ? (y - pa.y) / (pb.y - pa.y) : 1;
+      let gradient2 = pc.y != pd.y ? (y - pc.y) / (pd.y - pc.y) : 1;
+
+      let sx = this.interpolate(pa.x, pb.x, gradient1) >> 0; //starting x
+      let ex = this.interpolate(pc.x, pd.x, gradient2) >> 0; //ending x
+
+      let z1: number = this.interpolate(pa.z, pb.z, gradient1);
+      let z2: number = this.interpolate(pc.z, pd.z, gradient2);
+
+      for (let x = sx; x < ex; x++) {
+        let gradient: number = (x - sx) / (ex - sx);
+        let z = this.interpolate(z1, z2, gradient);
+        this.drawPoint(new BABYLON.Vector3(x, y, z), color);
+      }
+    }
+
+    public drawTriangle(
+      p1: BABYLON.Vector3,
+      p2: BABYLON.Vector3,
+      p3: BABYLON.Vector3,
+      color: BABYLON.Color4
+    ): void {
+      if (p1.y > p2.y) {
+        var temp = p2;
+        p2 = p1;
+        p1 = temp;
+      }
+
+      if (p2.y > p3.y) {
+        var temp = p2;
+        p2 = p3;
+        p3 = temp;
+      }
+
+      if (p1.y > p2.y) {
+        var temp = p2;
+        p2 = p1;
+        p1 = temp;
+      }
+
+      let dP1P2: number;
+      let dP1P3: number;
+
+      if (p2.y - p1.y > 0) {
+        dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+      } else dP1P2 = 0;
+
+      if (p3.y - p1.y > 0) {
+        dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+      } else dP1P3 = 0;
+
+      if (dP1P2 > dP1P3) {
+        for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+          if (y < p2.y) {
+            this.processScanLine(y, p1, p3, p1, p2, color);
+          } else {
+            this.processScanLine(y, p1, p3, p2, p3, color);
+          }
         }
-        if (e2 < dx) {
-          err += dx;
-          y0 += sy;
+      } else {
+        for (var y = p1.y >> 0; y <= p3.y >> 0; y++) {
+          if (y < p2.y) {
+            this.processScanLine(y, p1, p2, p1, p3, color);
+          } else {
+            this.processScanLine(y, p2, p3, p1, p3, color);
+          }
         }
       }
     }
+
+    // public drawBLine(point0: BABYLON.Vector2, point1: BABYLON.Vector2): void {
+    //   let x0 = point0.x >> 0;
+    //   let y0 = point0.y >> 0;
+    //   let x1 = point1.x >> 0;
+    //   let y1 = point1.y >> 0;
+    //   let dx = Math.abs(x1 - x0);
+    //   let dy = Math.abs(y1 - y0);
+    //   let sx = x0 < x1 ? 1 : -1;
+    //   let sy = y0 < y1 ? 1 : -1;
+    //   let err = dx - dy;
+
+    //   while (true) {
+    //     this.drawPoint(new BABYLON.Vector2(x0, y0), new BABYLON.Color4(0,1,0,1));
+    //     if (x0 == x1 && y0 == y1) break;
+    //     let e2 = 2 * err;
+    //     if (e2 > -dy) {
+    //       err -= dy;
+    //       x0 += sx;
+    //     }
+    //     if (e2 < dx) {
+    //       err += dx;
+    //       y0 += sy;
+    //     }
+    //   }
+    // }
 
     public render(camera: Camera, meshes: Mesh[]) {
       let viewMat = BABYLON.Matrix.LookAtLH(
@@ -165,10 +266,16 @@ module SoftEngine {
           let pixelA = this.project(vertexA, transformMat);
           let pixelB = this.project(vertexB, transformMat);
           let pixelC = this.project(vertexC, transformMat);
-
-          this.drawBLine(pixelA, pixelB);
-          this.drawBLine(pixelB, pixelC);
-          this.drawBLine(pixelC, pixelA);
+          let color: number =
+            0.25 + ((j % cMesh.Faces.length) / cMesh.Faces.length) * 0.75;
+          this.drawTriangle(
+            pixelA,
+            pixelB,
+            pixelC,
+            new BABYLON.Color4(color, color, color, 1)
+          );
+          // this.drawBLine(pixelB, pixelC);
+          // this.drawBLine(pixelC, pixelA);
         }
       }
     }
@@ -248,7 +355,11 @@ module SoftEngine {
         }
 
         let position = jsonObject.meshes[meshI].position;
-        mesh.Position = new BABYLON.Vector3(position[0], position[1], position[2]);
+        mesh.Position = new BABYLON.Vector3(
+          position[0],
+          position[1],
+          position[2]
+        );
         meshes.push(mesh);
       }
       return meshes;
